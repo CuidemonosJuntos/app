ecf73d9f89a4750af356df7edc3b00a5c81e6f28
Paola Garay <garay.angela@gmail.com>
2020-10-17 00:01:59 +0200

    React Router setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div >
-    <h2>Hi there!</h2>
-          <a href="/auth/google">Sign In with Google</a>
-    </div>
-  );
+import * as actions from '../actions';
+import Header from './Header';
+import Landing from './Landing';
+
+const Dashboard = () => <h2>Dashboard</h2>
+const ServiceNew = () => <h2>ServiceNew</h2>
+
+class App extends Component {
+  componentDidMount() {
+      this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path="/" exact component={Landing}/>
+            <Route path="/dashboard" exact component={Dashboard}/>
+            <Route path="/dashboard/new" component={ServiceNew}/>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(null, actions)(App);
