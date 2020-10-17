7c9c1d9d0840e313aca1dd9759a5725f53a591f0
Paola Garay <garay.angela@gmail.com>
2020-10-17 13:07:22 +0200

    Complete Authentication flow in Client side and Create Header component

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+ function Landing() {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>
+                    Transform Your Life
+            </h1>
+            Learn from our community of indigenas who leading personal growth.
+        </div>
+    )
+}
+
+export default Landing;
